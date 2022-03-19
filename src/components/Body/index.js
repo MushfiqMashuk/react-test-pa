58b91946b4186fa15efff2dc54a08cd3c00935f5
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-03-20 03:54:08 +0600

    Data Fetching

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,33 +1,68 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Ad from "../../styles/images/ad-banner.jpg";
 import ImageComponent from "../ImageComponent";
 import Title from "../Title";
 import "./body.scss";
 
 const Body = () => {
+  const [newsData, setNewsData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  console.log(newsData);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        setError("");
+
+        const response = await fetch("http://localhost:4000/news");
+
+        const data = await response.json();
+
+        console.log(data);
+
+        setLoading(false);
+        setNewsData(data);
+      } catch (err) {
+        setLoading(false);
+        setError("There is a server-side error!");
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div className="body_container">
-      <div>
-        <div className="news_container">
-          <div className="lead_news">
-            <div className="lead_news_container">Lead News</div>
-            <div className="image_conainer">Image</div>
-          </div>
-          <div className="side_ad">2</div>
-          <div className="second_news">3</div>
-          <div className="left_column">
-            <div className="news_without_photo">
-              <Title supTitle="Alzaziara">This is a title</Title>
+    <>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!loading && !error && (
+        <div className="body_container">
+          <div>
+            <div className="news_container">
+              <div className="lead_news">
+                <div className="lead_news_container">Lead News</div>
+                <div className="image_conainer">Image</div>
+              </div>
+              <div className="side_ad">2</div>
+              <div className="second_news">3</div>
+              <div className="left_column">
+                <div className="news_without_photo">
+                  <Title supTitle="Alzaziara">This is a title</Title>
+                </div>
+                <div className="news_with_photo"></div>
+              </div>
+              <div className="right_column">5 </div>
             </div>
-            <div className="news_with_photo"></div>
+            <div className="ad_container">
+              <ImageComponent src={Ad} alt="Ad Banner" className="img" />
+            </div>
           </div>
-          <div className="right_column">5 </div>
         </div>
-        <div className="ad_container">
-          <ImageComponent src={Ad} alt="Ad Banner" className="img" />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
