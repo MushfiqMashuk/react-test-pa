1a7606adb7cc8c40ab6e4483530282d1927fe83d
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-03-19 21:34:46 +0600

    Layout Design

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -2,11 +2,31 @@ import React from "react";
 import "./body.scss";
 
 const Body = () => {
-  return <div className="body-container">
-
-    
-
-  </div>;
+  return (
+    <div className="body_container">
+      <div>
+        <div className="news_container">
+          <div className="lead_news" style={{ backgroundColor: "#EE4023" }}>
+            <div className="lead_news_container">Lead News</div>
+            <div className="image_conainer">Image</div>
+          </div>
+          <div className="news" style={{ backgroundColor: "#b72da0" }}>
+            2
+          </div>
+          <div className="news" style={{ backgroundColor: "#31aebc" }}>
+            3
+          </div>
+          <div className="news" style={{ backgroundColor: "#725c6e" }}>
+            4
+          </div>
+          <div className="news" style={{ backgroundColor: "#5d7ad3" }}>
+            5{" "}
+          </div>
+        </div>
+        <div className="ad_container">Ad Container</div>
+      </div>
+    </div>
+  );
 };
 
 export default Body;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className="footer-container">Footer</div>;
 };
 
 export default Footer;
