c2ac628852103a1db35b586a079604e3a8ca348b
Mushfiq Mashuk <alberteinstein3626@gmail.com>
2022-03-19 22:02:54 +0600

    Layout Complete

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -6,22 +6,14 @@ const Body = () => {
     <div className="body_container">
       <div>
         <div className="news_container">
-          <div className="lead_news" style={{ backgroundColor: "#EE4023" }}>
+          <div className="lead_news">
             <div className="lead_news_container">Lead News</div>
             <div className="image_conainer">Image</div>
           </div>
-          <div className="news" style={{ backgroundColor: "#b72da0" }}>
-            2
-          </div>
-          <div className="news" style={{ backgroundColor: "#31aebc" }}>
-            3
-          </div>
-          <div className="news" style={{ backgroundColor: "#725c6e" }}>
-            4
-          </div>
-          <div className="news" style={{ backgroundColor: "#5d7ad3" }}>
-            5{" "}
-          </div>
+          <div className="side_ad">2</div>
+          <div className="second_news">3</div>
+          <div className="left_column">4</div>
+          <div className="right_column">5 </div>
         </div>
         <div className="ad_container">Ad Container</div>
       </div>
